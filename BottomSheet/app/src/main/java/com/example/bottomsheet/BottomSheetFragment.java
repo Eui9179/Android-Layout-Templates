e446ee4c2a326d95b70709f7157696446e0e1c35
eui9179 <outdark97@gmail.com>
2022-05-22 15:25:07 +0900

    feature/listener interface

--- BottomSheet/app/src/main/java/com/example/bottomsheet/BottomSheetFragment.java
+++ BottomSheet/app/src/main/java/com/example/bottomsheet/BottomSheetFragment.java
@@ -1,14 +1,18 @@
 package com.example.bottomsheet;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +25,10 @@ import org.w3c.dom.Text;
 import java.util.Objects;
 
 public class BottomSheetFragment extends BottomSheetDialogFragment {
+    public interface InputListener {
+        void sendInput(String output);
+    }
+    public InputListener mInputListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +40,30 @@ public class BottomSheetFragment extends BottomSheetDialogFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Bundle bundle = getArguments();
-        TextView text = view.findViewById(R.id.text);
-        text.setText(bundle.getString("test"));
+
+        TextView text = view.findViewById(R.id.text_frag);
+        Button buttonSend = view.findViewById(R.id.btn_send);
+        EditText editInput = view.findViewById(R.id.edit_frag);
+
+
+        text.setText(bundle.getString("inputText"));
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mInputListener.sendInput(editInput.getText().toString());
+                dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            mInputListener = (InputListener) getActivity();
+        } catch (ClassCastException e) {
+            Log.e("ClassCastException", e.getMessage());
+        }
     }
 }
--- BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.java
+++ BottomSheet/app/src/main/java/com/example/bottomsheet/MainActivity.java
@@ -7,19 +7,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 import java.util.zip.Inflater;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomSheetFragment.InputListener{
     BottomSheetFragment bottomSheetFragment;
+    EditText inputText;
+    TextView outputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputText = findViewById(R.id.edit_main);
+        outputText = findViewById(R.id.text_main);
 
         findViewById(R.id.btn_open).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,11 +33,17 @@ public class MainActivity extends AppCompatActivity {
                 bottomSheetFragment = new BottomSheetFragment();
 
                 Bundle bundle = new Bundle();
-                bundle.putString("test","test");
+
+                bundle.putString("inputText", inputText.getText().toString());
                 bottomSheetFragment.setArguments(bundle);
 
                 bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
             }
         });
+    }
+
+    @Override
+    public void sendInput(String output) {
+        outputText.setText(output);
     }
 }
